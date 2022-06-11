9dd77f63ab2727a32e6e0ffdfa7afcf0c934e9d2
wom987 <45610462+wom987@users.noreply.github.com>
2022-06-11 14:42:22 -0600

    datatable added to project

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function () {
+Route::middleware(['auth', 'role:auditor'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', function () {
         return "this a response";
     })->name('index');
