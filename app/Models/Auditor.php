2ab86d30f63c4f06b606898d77178925a21726bc
wom987 <45610462+wom987@users.noreply.github.com>
2022-06-11 14:23:48 -0600

    register done

--- app/Models/Auditor.php
+++ app/Models/Auditor.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Auditor extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'dui',
+        'nit',
+        'address',
+        'id_usuario',
+    ];
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
                                 type="text"
                                 hidden
                                 name="role"
-                                value="user">
+                                value="seller">
                             <div class="row mb-3">
                                 <label for="name"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
